d12d930c54103ed04835e4ad3782b6c9eabe11bb
vandung <xyzvandung2009@gmail.com>
2025-01-19 21:59:45 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showPage(pageId) {
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => {
+        page.classList.remove('active');
+    });
+    document.getElementById(pageId).classList.add('active');
+}
