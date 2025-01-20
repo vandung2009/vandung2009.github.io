1cf35af33105c355ec763a054860e6426c333fd6
vandung <xyzvandung2009@gmail.com>
2025-01-20 10:12:26 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Thêm class "active" vào menu hiện tại
-const navLinks = document.querySelectorAll('.nav-link');
-navLinks.forEach(link => {
-    if (link.href === window.location.href) {
-        link.classList.add('active');
-    }
-});
